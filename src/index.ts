6a0fd05c04c9c8ab6ae2572c687af0c07e93ca43
Andrew Garcia <andrew@andrewgarcia.me>
2024-09-30 12:33:37 -0700

    Refactor main function

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,8 @@ import isEqual from "lodash/isEqual";
 const OBSIDIAN_PATH =
   "/Users/andrewgarcia/Library/Mobile Documents/iCloud~md~obsidian/Documents/Andrew/Notes/What makes a pragmatic programmer?.md";
 
-async function main() {
-  const { content, deck, name } = await source.parseMd(OBSIDIAN_PATH);
-  const htmlContent = await marked(content);
-
-  const fields: anki.BasicNoteFields = {
-    Front: name,
-    Back: htmlContent,
-  };
-
+async function upsertNote(fields: anki.BasicNoteFields, deck: string): Promise<void> {
+  log.info(`Upserting note: "${fields.Front}"`);
   const existingNote = await anki.findNoteByQuery({ front: fields.Front, deck, note: "Basic" });
   if (existingNote) {
     if (isEqual(existingNote.fields, fields)) {
@@ -28,6 +21,18 @@ async function main() {
     await anki.createNote(fields, deck);
     log.info(`Created new note: "${fields.Front}"`);
   }
+}
+
+async function main() {
+  const { content, deck, name } = await source.parseMd(OBSIDIAN_PATH);
+  const htmlContent = await marked(content);
+
+  const fields: anki.BasicNoteFields = {
+    Front: name,
+    Back: htmlContent,
+  };
+
+  await upsertNote(fields, deck);
 }
 
 main().catch((e) => log.error(e));
